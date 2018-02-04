a02b69f9c8dfd35ad17587ffabac8fc744801516
Weston Steimel <weston.steimel@gmail.com>
2018-02-04 22:37:16 +0000

    Created dockerfiles for python3.6.4 and ql versions 1.12 and 1.11.
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-versions=( "$@" )
-if [ ${#versions[@]} -eq 0 ]; then
-	versions=( */ )
+ql_versions=( "$@" )
+if [ ${#ql_versions[@]} -eq 0 ]; then
+	ql_versions=( */ )
 fi
-versions=( "${versions[@]%/}" )
+ql_versions=( "${ql_versions[@]%/}" )
 
 # see http://stackoverflow.com/a/2705678/433558
 sed_escape_lhs() {
@@ -15,22 +15,28 @@ sed_escape_rhs() {
 	echo "$@" | sed -e 's/[\/&]/\\&/g' | sed -e ':a;N;$!ba;s/\n/\\n/g'
 }
 
+python_versions=(3.6.4)
 alpine_versions=(3.6 3.7)
 
-for version in "${versions[@]}"; do
-    echo "Generating Dockerfiles for Boost version ${version}."
-    template=alpine
-    source $version/CONFIG
-    echo "Generating templates for ${template}"
+for ql_version in "${ql_versions[@]}"; do
+    for python_version in ${python_versions[@]}; do
+        echo "Generating Dockerfiles for QuantLib version ${ql_version} and Python version ${python_version}."
+        template=alpine
+        echo "Generating templates for ${template}"
+        python_lib_path=python${python_version:0:3}
 
-    for alpine_version in ${alpine_versions[@]}; do
-	mkdir -p $version/$template/$alpine_version
-	tag=$boost_version-$template$alpine_version
-        
-	sed -r \
-	    -e 's!%%TAG%%!'"$tag"'!g' \
-	    -e 's!%%QUANTLIB_VERSION%%!'"$version"'!g' \
-            "Dockerfile-${template}.template" > "$version/$template/$alpine_version/Dockerfile"
-	echo "Generated ${version}/${template}/${alpine_version}/Dockerfile"
+        for alpine_version in ${alpine_versions[@]}; do
+	    mkdir -p $ql_version/python-$python_version/$template/$alpine_version
+	    ql_builder_tag=$ql_version-$template$alpine_version
+            python_tag=$python_version-$template$alpine_version
+
+	    sed -r \
+	        -e 's!%%QL_BUILDER_TAG%%!'"$ql_builder_tag"'!g' \
+		-e 's!%%PYTHON_TAG%%!'"$python_tag"'!g' \
+	        -e 's!%%QUANTLIB_SWIG_VERSION%%!'"$ql_version"'!g' \
+		-e 's!%%PYTHON_LIB_PATH%%!'"$python_lib_path"'!g' \
+                "Dockerfile-${template}.template" > "$ql_version/python-$python_version/$template/$alpine_version/Dockerfile"
+	    echo "Generated ${ql_version}/python-${python_version}/${template}/${alpine_version}/Dockerfile"
+        done
     done
 done
