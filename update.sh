94c1562e139dcb761ef60168e39068417953580c
Weston Steimel <weston.steimel@gmail.com>
2018-02-19 23:51:15 +0000

    Add stuff for python 3.5
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- update.sh
+++ update.sh
@@ -15,15 +15,27 @@ sed_escape_rhs() {
 	echo "$@" | sed -e 's/[\/&]/\\&/g' | sed -e ':a;N;$!ba;s/\n/\\n/g'
 }
 
-python_versions=(3.6.4)
-alpine_versions=(3.6 3.7)
+declare -A python_alpine_versions
+python_alpine_versions=( ["3.6.4"]="3.4;3.6;3.7" ["3.5.5"]="3.4")
+
+#for i in "${pages[@]}"
+#do
+#        arr=(${i//;/ })
+#        site=${arr[0]}
+#        language=${arr[1]}
+#        protocol=${arr[2]}
+#done
+
+#python_versions=(3.6.4 3.5.5)
+#alpine_versions=(3.6 3.7)
 
 for ql_version in "${ql_versions[@]}"; do
-    for python_version in ${python_versions[@]}; do
+    for python_version in ${!python_alpine_versions[@]}; do
         echo "Generating Dockerfiles for QuantLib version ${ql_version} and Python version ${python_version}."
         template=alpine
         echo "Generating templates for ${template}"
         python_lib_path=python${python_version:0:3}
+	alpine_versions=(${python_alpine_versions[$python_version]//;/ })
 
         for alpine_version in ${alpine_versions[@]}; do
 	    dockerfile_path=$ql_version/python$python_version/$template/$alpine_version 
